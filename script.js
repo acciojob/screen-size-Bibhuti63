9864a3d8725c8deed77cd29e2dd7c53a6aae74c5
Bibhuti Biswal <91078576+Bibhuti63@users.noreply.github.com>
2023-06-10 12:22:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+// const element=document.getElementById('div1');
+// element.innerHTML="Hello World";
+
+	
+
+      findSize();
+
+      window.addEventListener("resize", findSize);
+
+	function findSize() {
+        var width1 = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        var height1 = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+        var size = document.getElementById("id1");
+        size.innerHTML = "<h1>Width: " + width1 + " and Height: " + height1 + "</h1>";
+	}
